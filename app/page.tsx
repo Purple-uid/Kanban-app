049b9f8ae4fd3a7ad5eb338b7d41a353c813a929
Purple <dio30435@gmail.com>
2026-06-24 01:11:08 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Board from "../components/Board/Board";
 
 export default function Home() {
   return <div>
-    <h1 className="text-black text-7xl font-bold text-center py-6 bg-gray-200">UID_Kanban</h1>
+    <h1 className="text-black text-7xl font-bold text-center py-6 bg-gray-200">FLUID_Kanban</h1>
     <div className="flex overflow-x-auto gap-4 p-4 scrollbar-thin snap-x snap-mandatory">
       <Board />
     </div>
